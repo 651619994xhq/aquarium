e4b09300f692db4db7bcbedbdff204f3d472c17c
xionghq <18614084016@163.com>
2019-08-29 19:45:47 +0800

    提交代码

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,8 +5,6 @@ import {getMyLabel,getIndexInfo} from '../../utils/service'
 import {hideLoading, showLoading} from '../../utils/http';
 Page({
     data: {
-        test:'123',
-        num:10,
         navData:[
             {
                 name: '关注',
@@ -43,64 +41,41 @@ Page({
         selectIndex:0,
         selectId:'fllow',
         currentTab: 0,
-        windowHeight:0,
-        windowWidth:0,
-        scrollHeight:0,
         currentPage: 1,
         hasMore:false,
         hasRefesh:false,
+        backTop:false,
         list:[], //
-    },
-    onLoad: function () {
-        this.init();
-    },
-    calScrollviewHeight(){
-        let that=this;
-        var obj = wx.createSelectorQuery();
-            obj.select('.fill-bank').boundingClientRect();
-            obj.exec(function (rect) {
-                //这里减去顶部的高度和底部的高度
-                that.setData({
-                    scrollHeight:that.data.windowHeight-rect[0].height-64
-                })
-                console.log('windowHeight',that.data.windowHeight,' rect-height==>',rect[0].height,' scollHeight==>',that.data.scrollHeight)
-            }) ;
 
+    },
+    onLoad() {
+        this.init();
     },
     async init(){
         //这个是小程序的bug
         wx.setBackgroundColor({
             backgroundColor: '#ffffff', // 窗口的背景色为白色
         });
+        await this.getMyLikeLabel();
+        await this.getPageInfoWithParam();
+    },
+    //获取我喜欢的标签
+    async getMyLikeLabel(){
         await showLoading();
-        //获取信息
-        wx.getSystemInfo({
-            success: async (res)=>{
-                // 高度,宽度 单位为px
-                this.setData({windowHeight: res.windowHeight,windowWidth: res.windowWidth})
-                this.calScrollviewHeight();
-            }
-        });
         let [err,data]=await getMyLabel(); //获取头部标签信息
-        if(err!=null){wx.showToast({title: '系统错误'})};
-        this.setData({
-            navData:[{name: '关注', id:'fllow'}, {name: '推荐', id:'0'},...data]
-        });
-        await this.getPageInfoWithParam({page:1});
+        if(err!=null){wx.showToast({title: '系统错误'});await hideLoading();return false};
+        this.setData({ navData:[{name: '关注', id:'fllow'}, {name: '推荐', id:'0'},...data]});
         await hideLoading();
+        return true;
     },
     //获取页面信息 根据参数
-    async getPageInfoWithParam(param={}){
+    async getPageInfoWithParam(){
         await showLoading();
-        let [err,data]=await getIndexInfo({label: this.data.selectId,type:this.data.selectType,...param});
-        if(err!=null){wx.showToast({title:'系统错误'});hideLoading();return };
-        this.setData({
-            list:[...data.data]
-        })
+        let [err,data]=await getIndexInfo({label: this.data.selectId,type:this.data.selectType,page:this.data.currentPage});
+        if(err!=null){wx.showToast({title:'系统错误'});await hideLoading();return false};
+        this.setData({list:[...data.data]});
         await hideLoading();
-    },
-    onReady(){
-      //页面渲染完成
+        return true;
     },
     async onShow(){
       //页面显示
@@ -109,39 +84,54 @@ Page({
                selected:0
             })
         };
-        let [err,data]=await getMyLabel(); //获取头部标签信息
-        if(err!=null){wx.showToast({title: '系统错误'})};
-        let navData=[{name: '关注', id:'fllow'}, {name: '推荐', id:'0'},...data];
+        await this.getMyLikeLabel();
+    },
+    async switchNav(event){
+        await showLoading();
+        var cur = event.currentTarget.dataset.current;
+        //每个tab选项宽度占1/6
+        var singleNavWidth = this.data.windowWidth / 6;
+        //tab选项居中
+        this.setData({navScrollLeft: (cur - 2) * singleNavWidth});
+        if (this.data.currentTab == cur) {
+            await hideLoading();
+            return false;
+        };
+        this.setData({currentTab: cur,selectId:this.data.navData[cur]['id'],currentPage:1})
+        await this.getPageInfoWithParam();
+        await hideLoading();
+    },
+    async handleSelect(e){
+        let index=e.currentTarget.dataset.current;
+        if(index==this.data.selectIndex){
+            return ;
+        };
+        await showLoading();
+        this.data.selectList[this.data.selectIndex].select=false;
+        this.data.selectList[index].select=!this.data.selectList[index].select;
+        let selectType=this.data.selectList[index].type,
+        selectIndex=index;
         this.setData({
-            navData,
+            selectList:[...this.data.selectList],
+            selectType,
+            selectIndex,
+            currentPage:1
         });
+        await this.getPageInfoWithParam();
     },
-    onHide(){
-      //页面影藏
-    },
-    onUnload(){
-      //页面关闭
-    },
-    //点击事件处理
-    bindViewTap(){
-
-    },
-    //加载更多
-    loadMore:util.throttle(async function(){
+    async onReachBottom(){
         await showLoading();
         this.setData({
             hasMore:true,
             hasRefesh:false,
         });
-        let [err,data]=await getIndexInfo({page:this.data.currentPage+1});
+        let [err,data]=await getIndexInfo({page:this.data.currentPage+1,label: this.data.selectId,type:this.data.selectType,});
         if(err!=null){wx.showToast('系统异常'); hideLoading();return ;};
         if(data.data.length==0){
-            setTimeout(async ()=>{
                 this.setData({
                     hasMore:false
                 })
                 await hideLoading();
-            },1000)
             return ;
         };
         this.setData({
@@ -151,100 +141,52 @@ Page({
             hasRefesh:false
         });
         await hideLoading();
-    }),
-    //刷新
-    refesh:util.throttle(async function(){
+    },
+    async onPullDownRefresh(){
         await showLoading();
         this.setData({
             hasMore:false,
             hasRefesh:true,
         });
-        let [err,data]=await getIndexInfo({page:1});
+        let [err,data]=await getIndexInfo({page:1,label: this.data.selectId,type:this.data.selectType});
         if(JSON.stringify(data.data)==JSON.stringify(this.data.list)){
-            setTimeout(async ()=>{
                 this.setData({hasMore:false,hasRefesh:false,});
                 await hideLoading();
-            },1500);
-           return ;
+                wx.stopPullDownRefresh();
+            return ;
         };
         if(err!=null){wx.showToast('系统异常'); hideLoading();return ;};
-        setTimeout(async ()=>{
             this.setData({
                 currentPage:1,
                 list:[...data.data],
                 hasMore:false,
                 hasRefesh:false
             });
-            await hideLoading();
-        },2000)
-    }),
-    async switchNav(event){
-        await showLoading();
-        var cur = event.currentTarget.dataset.current;
-        //每个tab选项宽度占1/6
-        var singleNavWidth = this.data.windowWidth / 6;
-        //tab选项居中
-        this.setData({
-            navScrollLeft: (cur - 2) * singleNavWidth
-        });
-        // console.log(this.navScrollLeft)
-        if (this.data.currentTab == cur) {
-            await hideLoading();
-            return false;
+        await hideLoading();
+        wx.stopPullDownRefresh();
+    },
+    onPageScroll: function(e) {
+        if (e.scrollTop > 1000) {
+            this.setData({
+                backTop: true
+            })
         } else {
             this.setData({
-                currentTab: cur,
-                selectId:this.data.navData[cur]['id']
+                backTop: false
             })
-        };
-        await this.getPageInfoWithParam({page:1});
-        await hideLoading();
+        }
     },
-    async handleSelect(e){
-        await showLoading();
-        console.log('e==>',e.currentTarget.dataset.current)
-        let index=e.currentTarget.dataset.current;
-        this.data.selectList[this.data.selectIndex].select=false;
-        this.data.selectList[index].select=!this.data.selectList[index].select;
-        let selectType=this.data.selectList[index].type,
-        selectIndex=index;
-        this.setData({
-            selectList:[...this.data.selectList],
-            selectType,
-            selectIndex
-        });
-        await this.getPageInfoWithParam({page:1});
+    backTopHandle() {
+        if (wx.pageScrollTo) {
+            wx.pageScrollTo({
+                scrollTop: 0,
+                duration: 500
+            })
+        } else {
+            wx.showModal({
+                title: '提示',
+                content: '当前微信版本过低，无法使用该功能，请升级到最新微信版本后重试。'
+            })
+        }
     },
-    //用这个刷新效果好一点 但是不能在scroll-view 不触发 暂时废弃
-    // async onPullDownRefresh(e){
-    //  console.log('onPullDownRefresh',e)
-    //     await showLoading();
-    //     this.setData({
-    //         hasMore:false,
-    //         hasRefesh:true,
-    //     });
-    //     let [err,data]=await getIndexInfo({page:1});
-    //     if(JSON.stringify(data.data)==JSON.stringify(this.data.list)){
-    //         setTimeout(async ()=>{
-    //             this.setData({hasMore:false,hasRefesh:false,});
-    //             await hideLoading();
-    //             wx.stopPullDownRefresh()
-    //         },1000);
-    //         return ;
-    //     };
-    //     if(err!=null){wx.showToast('系统异常'); hideLoading();return ;};
-    //     setTimeout(async ()=>{
-    //         this.setData({
-    //             currentPage:1,
-    //             list:[...data.data],
-    //             hasMore:false,
-    //             hasRefesh:false
-    //         });
-    //         await hideLoading();
-    //         wx.stopPullDownRefresh()
-    //     },2000)
-    // },
-    changeFn(e){
-        console.log('e==>',e.detail);
-    }
 })
